098ea1e592e5a23b8529f54106b038775bf3ecfe
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-03 21:54:29 -0600

    Added Arrays for usernames and ages

---
+++ apps.js
@@ -0,0 +1,5 @@
+// Array of usernames and ages
+
+let usernames = ["Jack","Lucas","Tom","Sally","Janice","Bill","Grant",
+        "Connie", "Kim", "Natalie"]
+let userAges = [32, 45, 14, 21, 45, 12, 16, 51, 9, 33]
