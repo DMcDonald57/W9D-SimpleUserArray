03ea33c300468df814fd8636d85ed94641f4a550
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-06 15:50:58 -0700

    need to remove comment block

--- apps.js
+++ apps.js
@@ -7,13 +7,13 @@ let userAges = [32, 45, 14, 21, 45, 12, 16, 51, 9, 33]
 // Creating a while loop for over and equal to 18
 // that will not work.  All names appear regardless
 
-// let counter = 0;
-// while (counter < userAges.length) {
-//         if ([userAges] >= 18, [counter]){
-//                 console.log(userNames[counter]);
-//         }
-//         counter = counter +1
-// }
+let counter = 0;
+while (counter < userAges.length) {
+        if ([userAges] >= 18, [counter]){
+                console.log(userNames[counter]);
+        }
+        counter = counter +1
+}
 
 // Creating a for loop for under 18 but it also does not work
 
