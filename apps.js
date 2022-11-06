461ff2c368cf38ad592a853ae62dfa120568643c
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-05 21:03:50 -0600

    Really close now to getting this to work

--- apps.js
+++ apps.js
@@ -6,8 +6,8 @@ let userAges = [32, 45, 14, 21, 45, 12, 16, 51, 9, 33]
 
 let counter = 0;
 while (counter < userAges.length) {
-        if (userAges >= 18, [counter]) {
-                console.log([userNames]);
+        if (userAges >= 18, [counter]){
+                console.log(userNames[counter]);
                 
         }
 
