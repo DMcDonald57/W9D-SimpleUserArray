df63d9f1cc17fcc2bddae6bb626c53bae37d3613
DMcDonald57 <d.mcdonald579@gmail.com>
2022-11-05 22:10:16 -0600

    Created for loop that will not work

--- apps.js
+++ apps.js
@@ -4,12 +4,23 @@ let userNames = ["Jack","Lucas","Tom","Sally","Janice","Bill","Grant",
         "Connie", "Kim", "Natalie"]
 let userAges = [32, 45, 14, 21, 45, 12, 16, 51, 9, 33]
 
-let counter = 0;
-while (counter < userAges.length) {
-        if (userAges >= 18, [counter]){
+// Creating a while loop for over and equal to 18
+// that will not work.  All names appear regardless of if
+
+// let counter = 0;
+// while (counter < userAges.length) {
+//         if ([userAges] >= 18, [counter]){
+//                 console.log(userNames[counter]);
+                
+//         }
+//         counter = counter +1
+// }
+
+// Creating a for loop for under 18 but it also does not work
+
+for (let counter = 0; counter < userAges.length; counter++) {
+        if (userAges < 18, [counter]) {
                 console.log(userNames[counter]);
                 
         }
-
-        counter = counter +1
 }
